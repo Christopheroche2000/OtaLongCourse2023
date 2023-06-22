afbeb16727bd1c1d3d6283be783ab86fce7fc620
Christopheroche2000 <christopheroche59@gmail.com>
2023-06-22 12:28:40 +0100

    init

---
+++ javascript/class/object/js. script.js
@@ -0,0 +1,10 @@
+const person ={
+    name:"james",
+    age:54, 
+    siblings:["mike", "mosese","matthe",]
+}
+if(person.hasOwnProperty("age")){
+console.log("age exist")
+}else{
+console.log("dont exist")
+}
